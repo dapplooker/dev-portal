ed5b2559e516223c91e33c0bfa29822779d18c98
Keshav Chamria <keshavchamria@gmail.com>
2024-10-25 19:00:30 +0530

    add extra line in the end

--- src/app/_components/ui/styledComponents/Container.styled.ts
+++ src/app/_components/ui/styledComponents/Container.styled.ts
@@ -2,25 +2,25 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
-    width: 100%;
-    padding-right: 15px;
-    padding-left: 15px;
-    margin-right: auto;
-    margin-left: auto;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
 
-    @media (min-width: 576px) {
-        max-width: 540px;
-    }
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
 
-    @media (min-width: 768px) {
-        max-width: 720px;
-    }
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
 
-    @media (min-width: 992px) {
-        max-width: 960px;
-    }
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
 
-    @media (min-width: 1200px) {
-        max-width: 1320px;
-    }
+  @media (min-width: 1200px) {
+    max-width: 1320px;
+  }
 `;
